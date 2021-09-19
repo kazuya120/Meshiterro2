2da12318a8714625bc0a292c21200538d697e186
kazuya120 <songlinheye2@gmail.com>
2021-09-19 01:49:55 +0000

    Initial Commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1126ad0036c072af647c99825a823a9a7283d401ee1cfaeca05c61aa62a92c6c3ebe3242bd2676cc176f9fe5bf481f896d8073b0e0d262c423051b5889f9ffcd'
